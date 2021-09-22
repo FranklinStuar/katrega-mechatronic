c13551806dc502fc04a2a7a193c826a9db30c942
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2021-09-22 01:25:30 -0500

    Web.php for products

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\ServiceController;
 
 
 Route::get('/', [HomeController::class, 'index']);
+Route::resource('products', ProductController::class);
+Route::resource('services', App\Http\Controllers\ServiceController::class);
